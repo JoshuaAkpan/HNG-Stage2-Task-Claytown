44d3886456120824a33c27269a042e45ae5623d0
JoshuaAkpan <joshuaakpan7@gmail.com>
2024-07-14 20:21:01 +0100

    .

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import axios from 'axios'
+import axios from "axios";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -11,8 +11,8 @@ import ProductProvider from "./contexts/ProductContext";
 // cart provider
 import CartProvider from "./contexts/CartContext";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
